f71abd303839f31ce433b37682b277e28c7082cc
Marco Deb <marco.deb@gmail.com>
2025-12-01 13:46:49 +0100

    Initial Commit

---
+++ BreatheSync iOS/BreatheSync_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreatheSync_iOSApp.swift
+//  BreatheSync iOS
+//
+//  Created by Marco Deb on 2025-12-01.
+//
+
+import SwiftUI
+
+@main
+struct BreatheSync_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
