cb3554c02035ba4d78fa7d9c3a20355092c1b33c
Marco Deb <marco.deb@gmail.com>
2025-12-03 16:32:30 +0100

    Fixed so that audio plays when screen is off

--- BreatheSync iOS/BreatheSync_iOSApp.swift
+++ BreatheSync iOS/BreatheSync_iOSApp.swift
@@ -4,11 +4,24 @@
 //
 //  Created by Marco Deb on 2025-12-01.
 //
-
+import AVFoundation
 import SwiftUI
 
 @main
 struct BreatheSync_iOSApp: App {
+    
+    
+    init() {
+        
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default, options: [.mixWithOthers])
+                try AVAudioSession.sharedInstance().setActive(true)
+            } catch {
+                print("Failed to set audio session category: \(error.localizedDescription)")
+            }
+        }
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
